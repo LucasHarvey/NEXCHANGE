355c6eef3dd397f91b17666ed68bc8d3d937b8d0
Zack Therrien <zackarytherrien@gmail.com>
2017-12-30 21:04:39 +0000

    Formatting + changing to proper directories

--- deployment/upload_courses.sql
+++ deployment/upload_courses.sql
@@ -1,6 +1,6 @@
 use nexchange;
 
-LOAD DATA INFILE '/home/ubuntu/workspace/deployment/upload.csv' 
+LOAD DATA INFILE '/var/lib/mysql-files/upload_courses.csv' 
     IGNORE 
     INTO TABLE courses 
     FIELDS TERMINATED BY ';' ;
