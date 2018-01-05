a0dcd2e18071050f6688cc4a9119dd902557dea5
Lucas Harvey <lucas.harvey10@gmail.com>
2018-01-05 20:07:29 +0000

    forgot this

--- v1/_navbar.php
+++ v1/_navbar.php
@@ -5,7 +5,7 @@ include_once "_generics.php";
 if ($_SERVER["REQUEST_METHOD"] == "GET"){
   include_once "NavBar/navbar_GET.php";
   $conn = database_connect();
-  $user_id = getUserFromToken($conn);
+  $user_id = getUserFromToken();
   echoSuccess($conn, getNavbarItems($conn, $user_id));
 }else{
   echoError(null, 404, "UnknownResourceMethod");
