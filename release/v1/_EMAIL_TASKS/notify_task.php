d3758f627ce49b3db181919f28c94ecacfbe21e1
Zack Therrien <zackarytherrien@gmail.com>
2018-01-22 21:29:33 +0000

    Add database logging to functions

--- release/v1/_EMAIL_TASKS/notify_task.php
+++ release/v1/_EMAIL_TASKS/notify_task.php
@@ -1,4 +1,6 @@
 <?php
+include_once "./_generic_email.php";
+include_once "../_database.php";
 /*
 This script will send notifications to students that a new note was uploaded.
 */
@@ -7,14 +9,16 @@ echo "Sending Email Task running...".PHP_EOL;
 if (php_sapi_name() == "cli") { //Was this script ran from the commandline ?! Only allow this script to run from the commandline.
                                 //We are not checking for credentials but we expect commandline is secure. If an intruder has cmd access
                                 //Everything is vulnerable
+    $conn = database_connect();
     
     $to      = 'zackarytherrien@gmail.com';
     $subject = 'A new note is available for download';
     $message = 'Some message about their note being available, who wrote it and for which courses.';
-    $headers = 'From: no-reply@nexchange.johnabbott.qc.ca' . "\r\n" .
-        'X-Mailer: PHP/' . phpversion();
     
-    mail($to, $subject, $message, $headers);
+    
+    
+    send_email($conn, $to, $subject, $message);
+    
     echo "Emails sent.".PHP_EOL;
 }
 echo "Email Task Ran.".PHP_EOL;
--- release/v1/_EMAIL_TASKS/temporary_password_task.php
+++ release/v1/_EMAIL_TASKS/temporary_password_task.php
@@ -1,21 +1,23 @@
 <?php
-$WEBSERVER_ADDRESS = "https://note-share-lucasharvey.c9users.io/html/";
 
-if(empty($email_password_email)){
-    echoError(null, 500, "InternalServerError", "No email sent to password reset task.");
+include_once("./_generic_email.php");
+
+function reset_password($conn, $email, $token){
+    if(empty($email)){
+        echoError(null, 500, "InternalServerError", "No email sent to temporary password task.");
+    }
+    if(empty($token)){
+        echoError(null, 500, "InternalServerError", "No token sent to temporary password task.");
+    }
+    
+    $WEBSERVER_ADDRESS = "https://note-share-lucasharvey.c9users.io/html/";
+    $link = $WEBSERVER_ADDRESS."passwordreset.html?q=".$email_password_token;
+
+    $subject = 'No-Reply: NEXCHANGE Account Created';
+    $message = "Your NEXCHANGE account was created! Your login ID is your student ID, and your temporary password is: $email_password_token<br> You can login at: <a href='$link'>$link</a>";
+    
+    send_email($conn, $email, $subject, $message);
 }
-if(empty($email_password_token)){
-    echoError(null, 500, "InternalServerError", "No token sent to password reset task.");
-}
-$link = $WEBSERVER_ADDRESS."login.html";
 
-$to      = $email_password_email;
-$subject = 'No-Reply: NEXCHANGE Account Created';
-$message = "Your NEXCHANGE account was created! Your login ID is your student ID, and your temporary password is: $email_password_token<br> You can login at: <a href='$link'>$link</a>";
-$headers = 'From: no-reply@nexchange.johnabbott.qc.ca' . "\r\n" . 'X-Mailer: PHP/' . phpversion();
-
-mail($to, $subject, $message, $headers);
-
-//TODO: Log me.
 
 ?>
