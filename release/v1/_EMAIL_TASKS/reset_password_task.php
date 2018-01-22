7b3259e4fd41fa81acf113dcd1793b8e20750834
Zack Therrien <zackarytherrien@gmail.com>
2018-01-22 21:29:33 +0000

    Add database logging to functions

--- release/v1/_EMAIL_TASKS/reset_password_task.php
+++ release/v1/_EMAIL_TASKS/reset_password_task.php
@@ -1,22 +1,21 @@
 <?php
-$WEBSERVER_ADDRESS = "https://note-share-lucasharvey.c9users.io/html/";
 
-$email_password_token;
-$email_password_email;
+include_once("./_generic_email.php");
 
-if(empty($email_password_email)){
-    echoError(null, 500, "InternalServerError", "No email sent to password reset task.");
+function reset_password($conn, $email, $token){
+    if(empty($email)){
+        echoError(null, 500, "InternalServerError", "No email sent to password reset task.");
+    }
+    if(empty($token)){
+        echoError(null, 500, "InternalServerError", "No token sent to password reset task.");
+    }
+    
+    $link = $WEBSERVER_ADDRESS."passwordreset.html?q=".$email_password_token;
+
+    $subject = 'No-Reply: Password Reset Request';
+    $message = "We've received a request to reset your account linked with this email. If this was not you, ignore this message. Follow this link to reset your password: <br> <a href='$link'>$link</a>";
+    
+    send_email($conn, $email, $subject, $message);
 }
-if(empty($email_password_token)){
-    echoError(null, 500, "InternalServerError", "No token sent to password reset task.");
-}
-$link = $WEBSERVER_ADDRESS."passwordreset.html?q=".$email_password_token;
-
-$to      = $email_password_email;
-$subject = 'No-Reply: Password Reset Request';
-$message = "We've received a request to reset your account linked with this email. If this was not you, ignore this message. Follow this link to reset your password: <br> <a href='$link'>$link</a>";
-$headers = 'From: no-reply@nexchange.johnabbott.qc.ca' . "\r\n" . 'X-Mailer: PHP/' . phpversion();
-
-mail($to, $subject, $message, $headers);
 
 ?>
