555122f4040f445cb6bc4313f60d5f6350b880e4
Zack Therrien <zackarytherrien@gmail.com>
2018-01-23 20:13:43 +0000

    Only allow students to set their notifications

--- release/v1/UserAccess/useraccess_PUT.php
+++ release/v1/UserAccess/useraccess_PUT.php
@@ -16,12 +16,12 @@ $selectVals = array($courseId, $user_id);
 if(!database_start_transaction($conn, true)){
 	echoError($conn, 500, "DatabaseInsertError", "Could not start transaction.");
 }
-if(database_get_row($conn, "SELECT user_id, course_id FROM user_access WHERE course_id=? AND user_id=?", "ss", $selectVals) == null){
+if(database_get_row($conn, "SELECT user_id, course_id FROM user_access WHERE course_id=? AND user_id=? AND role='STUDENT'", "ss", $selectVals) == null){
     echoError($conn, 403, "AuthorizationFailed");
 }
 
 $insertVals = array($notifications, $user_id, $courseId);
-database_update($conn, "UPDATE user_access SET notifications=? WHERE user_id=? AND course_id=? LIMIT 1", "iss", $insertVals);
+database_update($conn, "UPDATE user_access SET notifications=? WHERE user_id=? AND course_id=? AND role='STUDENT' LIMIT 1", "iss", $insertVals);
 $value = database_get_row($conn, "SELECT notifications FROM user_access WHERE course_id=? AND user_id=?", "ss", $selectVals);
 if(!database_commit($conn)){
 	if(!database_rollback($conn)){
