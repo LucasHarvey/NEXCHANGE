eb9ae059b9be0ba28193d9bcacfe99b953b7a717
Zack Therrien <zackarytherrien@gmail.com>
2018-01-23 19:25:08 +0000

    Remove too much info from header; only log if mail succeeded

--- release/v1/_EMAIL_TASKS/_generic_email.php
+++ release/v1/_EMAIL_TASKS/_generic_email.php
@@ -1,12 +1,8 @@
 <?php
-
 function send_email($conn, $userid, $to, $subject, $message){
-    $headers = 'From: no-reply@nexchange.johnabbott.qc.ca' . "\r\n" .
-        'X-Mailer: PHP/' . phpversion();
+    $headers = "From: no-reply.nexchange@johnabbott.qc.ca\r\n";
     
-    mail($to, $subject, $message, $headers);
-    
-    database_insert($conn, "INSERT INTO log_notifications_sent (user_id, notification_code) values (?,?)", "ss", array($userid, $ip));
+    if(mail($to, $subject, $message, $headers))
+        database_insert($conn, "INSERT INTO log_notifications_sent (user_id, notification_code) values (?,?)", "ss", array($userid, $ip));
 }
-
 ?>
