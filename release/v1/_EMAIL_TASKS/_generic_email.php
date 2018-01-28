2a89c861b9a8efd3fd6540d93b1e31b2bbbf78e3
Zack Therrien <zackarytherrien@gmail.com>
2018-01-28 07:07:50 +0000

    Fix email file paths; add return value to users_POST to let admin know if email was sent

--- release/v1/_EMAIL_TASKS/_generic_email.php
+++ release/v1/_EMAIL_TASKS/_generic_email.php
@@ -2,7 +2,10 @@
 function send_email($conn, $userid, $to, $subject, $message){
     $headers = "From: no-reply.nexchange@johnabbott.qc.ca\r\n";
     
-    if(mail($to, $subject, $message, $headers))
+    if(mail($to, $subject, $message, $headers)){
         database_insert($conn, "INSERT INTO log_notifications_sent (user_id, notification_code) values (?,?)", "ss", array($userid, 0));
+        return true;
+    }
+    return false;
 }
 ?>
