71b107a6925822078827568c9fdefce850f52974
Zack Therrien <zackarytherrien@gmail.com>
2018-01-28 07:10:33 +0000

    Fix password.php path to sub files; add missing message code; fix super global used in password_POST; fix js function names

--- release/v1/Password/password_POST.php
+++ release/v1/Password/password_POST.php
@@ -1,27 +1,27 @@
 <?php
 $conn = database_connect();
 
-if(!array_key_exists("studentId", $_GET)){
+if(!array_key_exists("studentId", $_JSON)){
     echoError($conn, 400, "MissingArgumentStudentId");
 }
 
-if(!array_key_exists("email", $_GET)){
+if(!array_key_exists("email", $_JSON)){
     echoError($conn, 400, "MissingArgumentEmail");
 }
 
-$studentId = $_GET["studentId"];
-$email = $_GET["email"];
+$studentId = $_JSON["studentId"];
+$email = $_JSON["email"];
 
 $select = "SELECT id,email FROM users WHERE login_id = ? AND email = ? AND privilege = 'USER' LIMIT 1";
 
 $user = database_get_row($conn, $select, "ss", array($studentId, $email));
-
-$length = 40;
+//TODO; user exists
+$length = 20;
 $true = true;
 $token = bin2hex(openssl_random_pseudo_bytes($length, $true));
 
 $update = "UPDATE users SET passresetcode=?, passresetcreated=NOW() WHERE id=? LIMIT 1";
-database_update($conn, $update, "ss", $token, $user["id"]);
+database_update($conn, $update, "ss", array($token, $user["id"]));
 
 include_once("../_EMAIL_TASKS/reset_password_task.php");
 reset_password_email($conn, $user['id'], $user['email'], $token);
