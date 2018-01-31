5ca136a6e278125141be6500e7d39f6e31cddfc5
Zack Therrien <zackarytherrien@gmail.com>
2018-01-31 15:39:32 +0000

    Fix varaiables passed to db_get_row; select users to send email to

--- release/v1/_EMAIL_TASKS/notify_upload_task.php
+++ release/v1/_EMAIL_TASKS/notify_upload_task.php
@@ -7,12 +7,14 @@ function notify_note_upload_email_task($conn, $users, $noteId){
         echoError(null, 500, "InternalServerError", "No note id sent to upload note notify task.");
     }
     
-    $note = database_get_row($conn, "SELECT n.name, description, n.taken_on, c.course_name, c.course_number FROM notes n INNER JOIN courses c ON n.course_id = c.id WHERE n.id=?", $noteId);
+    $note = database_get_row($conn, "SELECT n.name, description, n.taken_on, c.course_name, c.course_number, n.course_id FROM notes n INNER JOIN courses c ON n.course_id = c.id WHERE n.id=?", "s", $noteId);
+    
+    $users = database_get_all($conn, "SELECT u.id, u.email FROM users u INNER JOIN user_access ua ON ua.user_id = u.id WHERE ua.course_id=?", "s", $note['course_id']);
     
     $link = $GLOBALS['NEXCHANGE_DOMAIN'] . "/home.html";
     
     $subject = 'No-Reply: NEXCHANGE - Notes Uploaded';
-    $message = "New notes were uploaded by a note taker.\n\nName: ".$note['name'].
+    $message = "New notes were uploaded by a note taker.\n\nNote Details\n\nName: ".$note['name'].
         "\nDescription: ".$note['description'].
         "\nTaken On: ".$note['taken_on'].
         "\nFor: ".$note['course_name']."(".$note['course_number'].")\n\n You can login at: $link";
