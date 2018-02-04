210929daad08fed424fe2b007bac45253ee5a813
Zack Therrien <zackarytherrien@gmail.com>
2018-02-04 20:04:24 +0000

    Add notification codes

--- release/v1/_EMAIL_TASKS/reset_password_task.php
+++ release/v1/_EMAIL_TASKS/reset_password_task.php
@@ -15,7 +15,7 @@ function reset_password_email($conn, $userid, $email, $token){
     $subject = 'No-Reply: Password Reset Request';
     $message = "We've received a request to reset your account password linked with this email.\n\nThis request will expire after 15 minutes. Follow this link to reset your password: $link\n\nIf this was not you, ignore this message.";
     
-    return send_email($conn, $userid, $email, $subject, $message);
+    return send_email($conn, 3, $userid, $email, $subject, $message);
 }
 
 ?>
