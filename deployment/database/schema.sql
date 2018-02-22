f193d15f6ff88ca9cdd4a25470e8a904ffb6d244
Zack Therrien <zackarytherrien@gmail.com>
2018-02-22 06:54:53 +0000

    Fix upload errors with docx files

--- deployment/database/schema.sql
+++ deployment/database/schema.sql
@@ -93,7 +93,7 @@ CREATE TABLE notefiles (
     note_id CHAR(36) NOT NULL,
     file_name VARCHAR(100) NOT NULL,
     storage_name VARCHAR(100) NOT NULL,
-    type VARCHAR(30),
+    type VARCHAR(350),
     size int(11),
     md5 CHAR(32) NOT NULL, -- MD5s are always 32 chars.
 
