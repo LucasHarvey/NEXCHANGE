6d23d1fad45cb6f546aa1582e6fc6e2f1171b51c
Zack Therrien <zackarytherrien@gmail.com>
2018-02-24 18:31:21 +0000

    Move parse_courses.rb to courses; Add async to generic email; add notifiaction code +10 for async in schema.sql

--- release/v1/_EMAIL_TASKS/_generic_email.php
+++ release/v1/_EMAIL_TASKS/_generic_email.php
@@ -1,11 +1,26 @@
 <?php
-function send_email($conn, $notificationCode, $userid, $to, $subject, $message){
+function send_email($conn, $notificationCode, $userid, $to, $subject, $message, $async = false){
     $headers = "From: no-reply.nexchange@johnabbott.qc.ca\r\n";
     
-    if(mail($to, $subject, $message, $headers)){
-        database_insert($conn, "INSERT INTO log_notifications_sent (user_id, notification_code) values (?,?)", "si", array($userid, $notificationCode));
-        return true;
+    if(!$async){
+        if(mail($to, $subject, $message, $headers)){
+            database_insert($conn, "INSERT INTO log_notifications_sent (user_id, notification_code) values (?,?)", "si", array($userid, $notificationCode));
+            return true;
+        }
+        return false;
+    }else{
+        $cmd = "sendmail -oi -t <<____HERE
+$headers To: $email
+Subject: $subject
+
+$message
+____HERE";
+        database_insert($conn, "INSERT INTO log_notifications_sent (user_id, notification_code) values (?,?)", "si", array($userid, 10 + $notificationCode));
+        execInBackground($cmd);
     }
-    return false;
 }
+
+function execInBackground($cmd) { 
+    exec($cmd . " > /dev/null &");   
+} 
 ?>
