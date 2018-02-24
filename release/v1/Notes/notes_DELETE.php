4f2dfe1a399d535599ccf72ab6e2e54ba6d09ecb
Lucas Harvey <lucas.harvey10@gmail.com>
2018-02-24 21:26:19 +0000

    unlink files when deleted and fix update note

--- release/v1/Notes/notes_DELETE.php
+++ release/v1/Notes/notes_DELETE.php
@@ -27,6 +27,14 @@ if(database_get_row($conn, "SELECT id FROM notes WHERE id=?".$whereClause, $dele
     echoError($conn, 403, "AuthorizationFailed");
 }
 
+$oldFile = database_get_row($conn, "SELECT storage_name FROM notefiles WHERE note_id=? LIMIT 1", "s", $noteId);
+
+$storageName = $oldFile["storage_name"];
+
+// Delete the old file
+if(!file_exists($storageName) || !unlink($storageName))
+    echoError($conn, 404, "NoteFileDeleteFailure");
+
 database_delete($conn, "DELETE FROM notes WHERE id=?".$whereClause." LIMIT 1", $deleteType, $deleteVals);
 
 
