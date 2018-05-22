ffb4dfaf21deafd2c9f87af91bc7a774711e895c
Lucas Harvey <lucas.harvey10@gmail.com>
2018-05-22 20:12:26 +0000

    resolve quotations issue

--- release/v1/_EMAIL_TASKS/_generic_email.php
+++ release/v1/_EMAIL_TASKS/_generic_email.php
@@ -10,9 +10,9 @@ function send_email($conn, $notificationCode, $userid, $to, $subject, $message, 
         return false;
     }else{
         database_insert($conn, "INSERT INTO log_notifications_sent (user_id, notification_code) values (?,?)", "si", array($userid, 10 + $notificationCode));
-        //Replace all single quotes with escaped quote.
-        $subject = htmlentities($subject);
-        $message = htmlentities($message);
+        //Replace all single and double quotes with escaped quote.
+        $subject = htmlentities($subject, ENT_QUOTES);
+        $message = htmlentities($message, ENT_QUOTES);
         
         execInBackground("php _EMAIL_TASKS/_background_email.php $to '$subject' '$message'");
     }
