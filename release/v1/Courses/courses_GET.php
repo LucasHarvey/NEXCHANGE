d3a46b6478856166ad48921fa37d584195035452
Zack Therrien <zackarytherrien@gmail.com>
2018-05-25 06:07:05 +0000

    Remove all mentions of section start/end in Php

--- release/v1/Courses/courses_GET.php
+++ release/v1/Courses/courses_GET.php
@@ -8,9 +8,9 @@ if(getUserPrivilege() != "ADMIN"){
 
 $offset =  array_key_exists("page", $_GET) ? $_GET['page'] : 0;
 
-$allowedProps = array("teacherFullName", "courseName", "courseNumber", "sectionStart", "sectionEnd", "semester", "courseId", "id", "page", "section");
-$changesKeysRemap = array("teacherFullName" => "teacher_fullname", "courseName" => "course_name", "sectionStart" => "section_start", "sectionEnd" => "section_end", "courseNumber" => "course_number", "courseId" => "id", "page"=>NULL, "section"=>NULL);
-$columnWhereClause = array("teacher_fullname" => "like", "course_name" => "like", "course_number" => "like", "sectionStart" => "=","sectionEnd" => "=", "semester" => "like", "id" => "=");
+$allowedProps = array("teacherFullName", "courseName", "courseNumber", "section", "semester", "courseId", "id", "page", "section");
+$changesKeysRemap = array("teacherFullName" => "teacher_fullname", "courseName" => "course_name", "section" => NULL, "courseNumber" => "course_number", "courseId" => "id", "page"=>NULL);
+$columnWhereClause = array("teacher_fullname" => "like", "course_name" => "like", "course_number" => "like", "semester" => "like", "id" => "=");
 
 $whereStmt = generateWhereStatement($conn, $allowedProps, $changesKeysRemap, $columnWhereClause, $_GET);
 
@@ -19,7 +19,7 @@ $insertVals = array();
 if(!empty($whereStmt[1])){
     foreach ($whereStmt[1] as $k=>$value) {
         if(substr($k, 0, strlen("section")) === "section"){
-            $insertTypes = $insertTypes . "i";
+            $insertTypes = $insertTypes . "s";
             array_push($insertVals, $value);
         }elseif($k == "id"){
             $insertTypes = $insertTypes . "s";
@@ -32,21 +32,12 @@ if(!empty($whereStmt[1])){
 }
 
 $notesCount = "(SELECT count(*) FROM notes WHERE course_id = c.id) as notesAuthored";
-$selectQuery = "SELECT id, teacher_fullname as teacherFullName, course_name as courseName, course_number as courseNumber, section_start as sectionStart, section_end as sectionEnd, semester, created, $notesCount FROM courses c".$whereStmt[0];
-if(array_key_exists("section", $_GET) && !empty($_GET['section'])){
-    $whereCount = count(array_keys($whereStmt[1]));
-    if($whereCount == 0){
-        $selectQuery = $selectQuery . " WHERE";
-    }elseif($whereCount >= 1){
-        $selectQuery = $selectQuery . " AND";
-    }
-    $selectQuery = $selectQuery . " ? BETWEEN section_start AND section_end";
-    $insertTypes = $insertTypes . "i";
-    array_push($insertVals, $_GET['section']);
-}
+$selectQuery = "SELECT id, teacher_fullname as teacherFullName, course_name as courseName, course_number as courseNumber, section, semester, created, $notesCount FROM courses c".$whereStmt[0];
 $selectQuery = $selectQuery. " LIMIT ".$GLOBALS['PAGE_SIZES']." OFFSET ". ($offset * $GLOBALS['PAGE_SIZES']);
 
 $courses = database_get_all($conn, $selectQuery, $insertTypes, $insertVals);
+
+//TODO: Must filter by section. Must be done in PHP. Could be done in MySQL but lets not complicate things.
 
 if(in_array("id", array_keys($whereStmt[1]))){
     if(count($courses) == 0){
