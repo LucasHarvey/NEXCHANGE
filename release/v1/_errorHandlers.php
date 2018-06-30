1ff5f6aed24eba25739091eb9eb822c72aacc32e
Zack Therrien <zackarytherrien@gmail.com>
2018-06-30 17:46:59 +0000

    Fix error handlers crashing

--- release/v1/_errorHandlers.php
+++ release/v1/_errorHandlers.php
@@ -17,7 +17,7 @@ function echoError($conn, $status, $messageCode, $message = ""){
     $ignoreMsgCodes = ['AuthenticationExpired', 'AuthenticationFailed'];
     if(!in_array($messageCode, $ignoreMsgCodes)){
         $e = new Exception();
-        error_log(getExceptionTraceAsString($e));
+        error_log($e->getTraceAsString());
     }
     
     if($conn != null){ //would occur if error happened in a script without need of a DB...?!
@@ -38,47 +38,6 @@ function echoError($conn, $status, $messageCode, $message = ""){
     ));
     http_response_code($status);
     die();
-}
-
-function getExceptionTraceAsString($exception) {
-    try{
-        $rtn = "";
-        $count = 0;
-        foreach ($exception->getTrace() as $frame) {
-            $args = "";
-            if (isset($frame['args'])) {
-                $args = array();
-                foreach ($frame['args'] as $arg) {
-                    if (is_string($arg)) {
-                        $args[] = "'" . $arg . "'";
-                    } elseif (is_array($arg)) {
-                        $args[] = "Array";
-                    } elseif (is_null($arg)) {
-                        $args[] = 'NULL';
-                    } elseif (is_bool($arg)) {
-                        $args[] = ($arg) ? "true" : "false";
-                    } elseif (is_object($arg)) {
-                        $args[] = get_class($arg);
-                    } elseif (is_resource($arg)) {
-                        $args[] = get_resource_type($arg);
-                    } else {
-                        $args[] = $arg;
-                    }   
-                }   
-                $args = join(", ", $args);
-            }
-            $rtn .= sprintf( "#%s %s(%s): %s(%s)\n",
-                                     $count,
-                                     $frame['file'],
-                                     $frame['line'],
-                                     $frame['function'],
-                                     $args );
-            $count++;
-        }
-        return $rtn;
-    }catch(Exception $e){
-        return "Could not do stacktrace.".$e->getMessage();
-    }
 }
 
 function echoSuccess($conn, $successObj, $statusCode = 200){
