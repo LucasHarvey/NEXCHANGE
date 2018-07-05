ecf611183371f9315d0f23bd7ab3c24b1d3b125b
Lucas Harvey <lucas.harvey10@gmail.com>
2018-07-05 19:41:18 +0000

    duplication

--- deployment/database/schema.sql
+++ deployment/database/schema.sql
@@ -51,14 +51,6 @@ CREATE TABLE users (
     PRIMARY KEY (id)
 );
 
-CREATE TABLE log_notifications_sent (
-    user_id CHAR(36) NOT NULL,
-    notification_code INT(2), -- 1: notify students; 2: notify notetakers (reminder); 3: reset password; 4: temporary password, +10 for maybe (async)
-    sent_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-);
-
 -- Create the tables
 CREATE TABLE log_notifications_sent (
     user_id CHAR(36) NOT NULL,
