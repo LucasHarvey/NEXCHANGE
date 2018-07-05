ae9ecfdc5343e2939d03aac503414e3a20e001f0
Lucas Harvey <lucas.harvey10@gmail.com>
2018-07-05 16:50:33 +0000

    add march break validation and force semester start/end

--- release/v1/Semester/semester_PUT.php
+++ release/v1/Semester/semester_PUT.php
@@ -45,12 +45,28 @@ if($marchBreakStart != null && $marchBreakEnd != null){
     if(strtotime($marchBreakEnd) <= strtotime($marchBreakStart))
         echoError($conn, 400, "MarchBreakNotValid");
 }
+
+if($marchBreakStart != null && $semesterStart != null){
+    if(strtotime($marchBreakStart) < strtotime($semesterStart))
+        echoError($conn, 400, "MarchBreakStartNotValid");
+}
+
+if($marchBreakEnd != null && $semesterEnd != null){
+    if(strtotime($marchBreakEnd) > strtotime($semesterEnd))
+        echoError($conn, 400, "MarchBreakEndNotValid");
+}
+
+if($marchBreakEnd != null){
+    if(strtotime($marchBreakEnd) > strtotime($semesterEnd))
+        echoError($conn, 400, "MarchBreakEndNotValid");
+}
     
 if(!database_start_transaction($conn)){
 	echoError($conn, 500, "DatabaseUpdateError", "Could not start transaction.");
 }
 
 $semester = database_get_row($conn, "SELECT semester_code from semesters WHERE semester_code=?", "s", $semesterCode);
+
 
 if($semester == null)
     echoError($conn, 400, "SemesterUpdateFailedDNE");
