156b49570ff821e267909f9846e60cce7fc6c4fc
Lucas Harvey <lucas.harvey10@gmail.com>
2018-07-06 16:25:25 +0000

    change default season for semester upload

--- release/js/Components/addSemester.js
+++ release/js/Components/addSemester.js
@@ -47,10 +47,9 @@ app.addSemester = {
     
     getDefaultSeason: function() {
         let month = new Date().getMonth();
-        let today = new Date().getDate();
-        if (month >= 11) return 1; //intersession
-        if (month >= 0 && month < 5) return 2; //winter
-        if (month >= 5 && month < 8) return 3; //summer
+        if (month >= 10) return 1; //intersession
+        if (month >= 0 && month < 3) return 2; //winter
+        if (month >= 3 && month < 7) return 3; //summer
         return 0; //fall
     },
     
