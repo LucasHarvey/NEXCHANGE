366f4e41387fc21e5fb24bf29eaab9d1faafa2fa
Lucas Harvey <lucas.harvey10@gmail.com>
2018-07-06 16:28:12 +0000

    password is not THE_PASSWORD like other occurences

--- deployment/database/init.sql
+++ deployment/database/init.sql
@@ -1,6 +1,5 @@
 source deployment/database/schema.sql
 
 -- ADD THE ADMIN USER
--- Password is: THE_PASSWORD
 INSERT INTO users (login_id, privilege, first_name, last_name, passwordhash, last_login) 
     VALUES ("Admin01", "ADMIN", "Administrator", "Administrator", "$2y$10$O27XHDCMtLTGDebeNSG2M.LghLnAtlB1FKc4oEAipPJeEWORpX/S.", NOW());
