4e65057563f39798f5fa721e770b82e56acfd3e1
Zack Therrien <zackarytherrien@gmail.com>
2018-07-13 16:07:39 +0000

    Stats for current semester only

--- release/v1/Admin/user_statistics.sql
+++ release/v1/Admin/user_statistics.sql
@@ -1,4 +1,4 @@
-SELECT login_id, first_name, last_name, email, created, 
+SELECT login_id, first_name, last_name, email, u.created, 
     IFNULL(ua.roles, "N/A") as roles, 
     IFNULL(ua.courses, "N/A") as courses, 
     IFNULL(logins.logins_per_week, "N/A") as logins_per_week, 
@@ -9,14 +9,17 @@ SELECT login_id, first_name, last_name, email, created,
             IFNULL(st_downs.totalDownloads, 0), " / "),
             
             IFNULL(
-                (SELECT COUNT(*) as ST_availableNotes FROM user_access ua INNER JOIN notes n ON n.course_id=ua.course_id
+                (SELECT COUNT(*) as ST_availableNotes 
+                FROM user_access ua INNER JOIN notes n ON n.course_id=ua.course_id
                 WHERE ua.user_id=u.id AND n.user_id != u.id
+                AND 1=(SELECT ua.created BETWEEN _s.semester_start AND _s.semester_end FROM semesters _s ORDER BY _s.created DESC LIMIT 1)
+                AND 1=(SELECT n.created BETWEEN _s.semester_start AND _s.semester_end FROM semesters _s ORDER BY _s.created DESC LIMIT 1)
                 GROUP BY ua.user_id)
             , 0)
     ), "N/A") as ST_downloads
     
     FROM users u 
-    
+
     LEFT JOIN (
         SELECT user_id, GROUP_CONCAT(DISTINCT role SEPARATOR ' and ') as roles,
                 GROUP_CONCAT(DISTINCT c.course_number SEPARATOR ';') as courses
@@ -31,19 +34,22 @@ SELECT login_id, first_name, last_name, email, created,
                                         ORDER BY created ASC LIMIT 1)
                                 )/7, 0), 2) AS logins_per_week
         FROM log_user_logins lu
+        WHERE 1=(SELECT lu.login_at BETWEEN _s.semester_start AND _s.semester_end FROM semesters _s ORDER BY _s.created DESC LIMIT 1)
         GROUP BY lu.user_id
     ) as logins ON u.id=logins.user_id
     
     LEFT JOIN (
         SELECT ln.user_id, ln.notification_code, COUNT(*) as rems_sent
         FROM log_notifications_sent ln
+        WHERE 1=(SELECT ln.sent_at BETWEEN _s.semester_start AND _s.semester_end FROM semesters _s ORDER BY _s.created DESC LIMIT 1)
         GROUP BY ln.user_id, notification_code
         HAVING notification_code IN (2, 12)
     ) as nt ON u.id=nt.user_id
     
     LEFT JOIN (
         SELECT user_id, COUNT(*) as notes_uploaded
-        FROM notes 
+        FROM notes n
+        WHERE 1=(SELECT n.created BETWEEN _s.semester_start AND _s.semester_end FROM semesters _s ORDER BY _s.created DESC LIMIT 1)
         GROUP BY user_id
     ) as nt_notes ON u.id=nt_notes.user_id
     
@@ -51,6 +57,7 @@ SELECT login_id, first_name, last_name, email, created,
         SELECT nfd.user_id, COUNT(DISTINCT notefile_id) as totalDownloads
         FROM notefile_downloads nfd
         WHERE nfd.user_id != (SELECT n.user_id FROM notes n INNER JOIN notefiles nf ON n.id=nf.note_id WHERE nf.id=nfd.notefile_id)
+        AND 1=(SELECT nfd.downloaded_at BETWEEN _s.semester_start AND _s.semester_end FROM semesters _s ORDER BY _s.created DESC LIMIT 1)
         GROUP BY nfd.user_id
     ) as st_downs ON u.id=st_downs.user_id
     
